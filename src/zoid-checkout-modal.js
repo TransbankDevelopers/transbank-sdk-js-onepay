7f0d24b96b80b4cc0bad1d8aafb3e2eaf618625a
Gonzalo Castillo <gonzalo.castillo@continuum.cl>
2018-09-03 17:21:03 -0300

    Add Onepay.version on BASE_URL

--- src/zoid-checkout-modal.js
+++ src/zoid-checkout-modal.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-undef, no-unused-vars */
 const zoid = require('zoid');
+const Onepay = require('onepay');
 
-const BASE_URL = 'https://cdn.rawgit.com/TransbankDevelopers/transbank-sdk-js-onepay/v1.4.2/html';
+const BASE_URL = 'https://cdn.rawgit.com/TransbankDevelopers/transbank-sdk-js-onepay/v'+ Onepay.version + '/html';
 const IFRAME_PATH = BASE_URL + '/checkout.html';
 
 let CheckoutModal = zoid.create({
