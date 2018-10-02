cc7c6a84f8ac822c60e82ed716c8b2c9e1bb009b
Gonzalo Castillo <gonzalo.castillo@continuum.cl>
2018-10-02 15:17:01 -0300

    Change lib reference for test

--- src/zoid-checkout-modal.js
+++ src/zoid-checkout-modal.js
@@ -3,8 +3,8 @@ const zoid = require('zoid');
 const Onepay = require('onepay');
 
 // const BASE_URL = 'https://cdn.rawgit.com/TransbankDevelopers/transbank-sdk-js-onepay/v' +
-const BASE_URL = 'https://rawgit.com/TransbankDevelopers/transbank-sdk-js-onepay/bug/android-context-change' +
-  require('onepay-lib-version') + '/html';
+//   require('onepay-lib-version') + '/html';
+const BASE_URL = 'https://rawgit.com/TransbankDevelopers/transbank-sdk-js-onepay/bug/android-context-change/html';
 const IFRAME_PATH = BASE_URL + '/checkout.html';
 
 let CheckoutModal = zoid.create({
